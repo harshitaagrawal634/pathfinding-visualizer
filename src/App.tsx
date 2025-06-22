63656a6bff6a7e1ed9dda33a7ae6b950977ecc1f
harshita <harshita.agrawal634@gmail.com>
2025-06-23 03:37:15 +0530

    color changed

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <PathfindingProvider>
       <TileProvider>
         <SpeedProvider>
-          <div className="h-screen w-screen flex flex-col bg-gray-100">
+          <div className="h-screen w-screen flex flex-col bg-gray-300">
             <Nav isVisualizationRunningRef={isVisualizationRunningRef} />
             <Grid isVisualizationRunningRef={isVisualizationRunningRef} />
           </div>
